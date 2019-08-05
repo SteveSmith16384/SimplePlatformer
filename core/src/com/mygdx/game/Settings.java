1b1c74c1f3b508eb14d7fb0728eb64981ebbe52c
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2019-08-05 11:45:05 +0100

    Renamed project

--- core/src/com/mygdx/game/Settings.java
+++ core/src/com/mygdx/game/Settings.java
@@ -3,7 +3,7 @@ package com.mygdx.game;
 public final class Settings {
 	
 	public static final String VERSION = "0.01";
-	public static final boolean RELEASE_MODE = false;
+	public static final boolean RELEASE_MODE = true;
 	
 	// Debug
 	//public static final boolean DEBUG_COINS = !RELEASE_MODE && false;
